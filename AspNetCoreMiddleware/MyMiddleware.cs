936f96ad94dadbf6865cfee7a09060c5f94f9841
cesar.sanjose <cksanjose@gmail.com>
2018-06-30 15:00:32 -0500

    Initial Commit

---
+++ AspNetCoreMiddleware/MyMiddleware.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace AspNetCoreMiddleware
+{
+    public class MyMiddleware
+    {
+        private RequestDelegate _next;
+        private ILoggerFactory _loggerFactory;
+        private IOptions<MyMiddlewareOptions> _options;
+
+        public MyMiddleware(RequestDelegate next, ILoggerFactory loggerFactory, IOptions<MyMiddlewareOptions> options)
+        {
+            _next = next;
+            _loggerFactory = loggerFactory;
+            _options = options;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            _loggerFactory.AddConsole();
+            var logger = _loggerFactory.CreateLogger("My Own Logger");
+            logger.LogInformation("My middleware is handling the request");
+            await context.Response.WriteAsync(_options.Value.OptionOne + Environment.NewLine);
+            await context.Response.WriteAsync("My middleware is handling the request!" + Environment.NewLine);
+            await _next.Invoke(context);
+            await context.Response.WriteAsync("My middleware has completed handing the request!" + Environment.NewLine);
+
+            //pass data to other middleware
+            context.Items["message"] = "The weather today is hot!";
+        }
+    }
+
+    public class MyMiddlewareOptions
+    {
+        public string OptionOne { get; set; }
+    }
+    
+    public static class MyMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseMyMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<MyMiddleware>();
+        }
+    }
+
+}
---
+++ AspNetCoreMiddleware/Startup.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.Internal;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AspNetCoreMiddleware
+{
+    public class Startup
+    {
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddOptions();
+
+            var myMiddlewareOptions = Configuration.GetSection("MyMiddlewareOptionsSection");
+            services.Configure<MyMiddlewareOptions>(o => o.OptionOne = myMiddlewareOptions["OptionOne"]);
+        }
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appSettings.json", false, true);
+
+            Configuration = builder.Build();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Use(async (context, next) =>
+            {
+                await context.Response.WriteAsync("Hello from component one!" + Environment.NewLine);
+                await next.Invoke();
+                await context.Response.WriteAsync("Hello from component one again!" + Environment.NewLine + context.Items["message"]);
+            });
+
+            app.UseMyMiddleware();
+            
+            //Use Map
+            app.Map("/mymapbranch", appBuilder =>
+            {
+               appBuilder.Run(async (context) =>
+                   {
+                       await context.Response.WriteAsync("Greetings from my branch" + Environment.NewLine);
+                   }); 
+            });
+            
+            //Use MapWhen
+            app.MapWhen(context => context.Request.Query.ContainsKey("querybranch"), (appBuilder) =>
+            {
+               appBuilder.Run(async context =>
+                   {
+                       await context.Response.WriteAsync("You have arrived at your Map When Branch" + Environment.NewLine);
+                   }); 
+            });
+
+            app.Run(async (context) => { await context.Response.WriteAsync("Hello World!"+ Environment.NewLine); });
+        }
+    }
+}
